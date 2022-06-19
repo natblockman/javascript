b30f06502b1341bb2a4b39e25bccb730f2734b13
natblockman <48959497+natblockman@users.noreply.github.com>
2022-06-19 10:27:28 +0700

    EP1

---
+++ app.js
@@ -0,0 +1,7 @@
+document.write("<h1>Hello World</h1>")
+document.write("<p>Javascript เบื้องต้น</p>")
+
+alert("สวัสดี");
+console.log("Hello Javascript");
+console.error("Error");
+console.warn("Warning");
